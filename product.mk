70dc49f26d4ee29c58ed6fcc4d7ee9df99818bbf
Alexander Koskovich <akoskovich@pm.me>
2022-04-29 23:39:05 -0700

    sm8250-common: Remove is-board-platform-in-list calls
     * Too early.
    
    Change-Id: I3f058e0f60445a52236356b24e91dc7b0c573dcd

--- product.mk
+++ product.mk
@@ -1,16 +1,10 @@
 MSM_VIDC_TARGET_LIST := kona lito bengal
 
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
-
 #MM_CORE
 MM_CORE := libmm-omxcore
 MM_CORE += libOmxCore
 
 PRODUCT_PACKAGES += $(MM_CORE)
-
-endif
-
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)), true)
 
 MM_VIDEO := ExoplayerDemo
 MM_VIDEO += libc2dcolorconvert
@@ -24,8 +18,6 @@ MM_VIDEO += init.qti.media.sh
 PRODUCT_PACKAGES += $(MM_VIDEO)
 
 include hardware/qcom/media/conf_files/$(TARGET_BOARD_PLATFORM)/$(TARGET_BOARD_PLATFORM).mk
-
-endif
 
 #Vendor property to enable Codec2 for audio and OMX for Video
 PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=1
